fee618aff3d2947086657268fc18b9f371fd9f76
Musecreatives <paulsolaeniolawun@gmail.com>
2022-08-30 15:20:16 +0100

    added randomness

--- lib/pong.dart
+++ lib/pong.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ball.dart';
 import 'bat.dart';
+import 'dart:math';
 
 enum Direction { up, down, left, right }
 
@@ -15,6 +16,10 @@ Direction vDir = Direction.down;
 Direction hDir = Direction.right;
 
 class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
+  double randX = 1;
+  double randY = 1;
+  int score = 0;
+
   @override
   void dispose() {
     controller?.dispose();
@@ -38,8 +43,12 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
     animation = Tween<double>(begin: 0, end: 100).animate(controller!);
     animation?.addListener(() {
       safeSetState(() {
-        (hDir == Direction.right) ? posX = increment : posX = increment;
-        (vDir == Direction.down) ? posY = increment : posY = increment;
+        (hDir == Direction.right)
+            ? posX = ((increment * randX).roundToDouble())
+            : posX = ((increment * randX).roundToDouble());
+        (vDir == Direction.down)
+            ? posY = ((increment * randY).roundToDouble())
+            : posY = ((increment * randY).roundToDouble());
       });
       checkBorders();
     });
@@ -64,6 +73,10 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
       batHeight = height! / 20;
       return Stack(
         children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Positioned(top: 0, right: 24, child: Text('Score: $score')),
+          ),
           Positioned(
             top: posY,
             left: posX,
@@ -82,27 +95,70 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
     });
   }
 
+  void showMessage(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Game Over'),
+            content: const Text('Would you like to play again?'),
+            actions: <Widget>[
+              ElevatedButton(
+                child: const Text('Yes'),
+                onPressed: () {
+                  setState(() {
+                    posX = 0;
+                    posY = 0;
+                    score = 0;
+                  });
+                  Navigator.of(context).pop();
+                  controller?.repeat();
+                },
+              ),
+              ElevatedButton(
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   void checkBorders() {
     double diameter = 50;
 
     if (posX! <= 0 && hDir == Direction.left) {
       hDir = Direction.right;
+      randX = randomNumber();
     }
     if (posX! >= width! - diameter && hDir == Direction.right) {
       hDir = Direction.left;
+      randX = randomNumber();
     }
     if (posY! >= height! - diameter - batHeight! && vDir == Direction.down) {
       // check if the bat is here, otherwise the game is over
       if (posX! >= (batPosition! - diameter) &&
           posX! <= (batPosition! + batWidth! + diameter)) {
         vDir = Direction.up;
+        randY = randomNumber();
       } else {
         controller!.stop();
-        dispose();
+        showMessage(context);
       }
     }
     if (posY! <= 0 && vDir == Direction.up) {
       vDir = Direction.down;
+      randY = randomNumber();
+    }
+    if (posX! >= (batPosition! - diameter) &&
+        posX! <= (batPosition! + batWidth! + diameter)) {
+      vDir = Direction.up;
+      randY = randomNumber();
+      safeSetState(() {
+        score++;
+      });
     }
   }
 
@@ -118,5 +174,13 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
         function();
       });
     }
+  }
+
+  double randomNumber() {
+    // this is a number between 0.5 and 1.5
+    //to initialize the ball in a random position
+    var ran = Random();
+    int myNum = ran.nextInt(101);
+    return (50 + myNum) / 100;
   }
 }
